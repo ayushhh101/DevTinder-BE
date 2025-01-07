ba06724cfdb3482d1352844b47ff2f8bba2b782b
Ayush <ayushsawant28112005@gmail.com>
2025-01-07 19:41:18 +0530

    Security Fix

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const { userAuth } = require('./middlewares/auth');
 const cookieParser = require('cookie-parser');
 // Middleware to parse the incoming request body & it works for all the routes automatically
 const cors = require('cors');
+require('dotenv').config();
 
 app.use(cors({
   origin: 'http://localhost:5173',
